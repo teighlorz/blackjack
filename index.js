8c29e6ce1cabe6d600a892dd35a502eb17dd1e68
Taylor <132228170+teighlorz@users.noreply.github.com>
2024-06-29 10:41:51 -0500

    Updated HTML + CSS styling

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ let firstCard = 3
 let secondCard = 9
 let sum = firstCard + secondCard
 
+let hasBlackJack = false
+let isAlive = true
+
+let message = " "
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+} else if (sum === 21) {
+    message = "Wohoo! You've got blackjack!"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game!"
+    isAlive = false
+}
+
+console.log(message)
